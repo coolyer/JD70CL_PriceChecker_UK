5e25080bc7ad2f08a294e48f2840444f6792f1f9
coolyer <35643522+coolyer@users.noreply.github.com>
2023-04-11 04:37:36 +0100

    Code Clean up
    Removed 2 imports as they are not needed and were used earlier and never deleted.

--- Jack daniles 70cl prices.py
+++ Jack daniles 70cl prices.py
@@ -5,8 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
-from bs4 import BeautifulSoup
-import requests
 import time
 # set up the Chrome driver and options
 # Headless must be set to false as website detect the browser as a bot.
